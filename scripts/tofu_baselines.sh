2737fb1668b9415f5cab123a65137aa30f804946
tzpranto <md.toufikzaman@gmail.com>
2025-12-03 18:20:00 +0000

    baselines tested and scripts added

---
+++ scripts/tofu_baselines.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+models=(
+    "Llama-3.2-1B-Instruct"
+)
+trainers_experiments=(
+    "SIBL unlearn/tofu/sibl.yaml"
+#    "GradAscent unlearn/tofu/default.yaml"
+    "GradDiff unlearn/tofu/default.yaml"
+    "NPO unlearn/tofu/default.yaml"
+    "DPO unlearn/tofu/idk.yaml"
+    "RMU  unlearn/tofu/default.yaml"
+)
+splits=(
+    "forget05 holdout05 retain95"
+)
+
+
+per_device_train_batch_size=8 # on two gpus would make effective batch size 32
+gradient_accumulation_steps=1
+
+
+########################################################################################################################
+########################################### Unlearn TOFU models ########################################################
+########################################################################################################################
+
+
+for split in "${splits[@]}"; do
+    forget_split=$(echo $split | cut -d' ' -f1)
+    holdout_split=$(echo $split | cut -d' ' -f2)
+    retain_split=$(echo $split | cut -d' ' -f3)
+
+    for model in "${models[@]}"; do
+        for trainer_experiment in "${trainers_experiments[@]}"; do
+            trainer=$(echo $trainer_experiment | cut -d' ' -f1)
+            experiment=$(echo $trainer_experiment | cut -d' ' -f2)
+            
+            task_name=tofu_${model}_${forget_split}_${trainer} 
+            model_path=open-unlearning/tofu_${model}_full
+            echo ${task_name}: Unlearning ${model_path} using ${trainer}
+
+            # Unlearn
+            CUDA_VISIBLE_DEVICES=0 python src/train.py --config-name=unlearn.yaml \
+            experiment=${experiment} \
+            trainer=${trainer} \
+            task_name=${task_name} \
+            model=${model} \
+            forget_split=${forget_split} \
+            retain_split=${retain_split} \
+            model.model_args.pretrained_model_name_or_path=${model_path} \
+            retain_logs_path=saves/eval/tofu_${model}_${retain_split}/TOFU_EVAL.json \
+            trainer.args.per_device_train_batch_size=$per_device_train_batch_size \
+            trainer.args.gradient_accumulation_steps=$gradient_accumulation_steps \
+            trainer.args.gradient_checkpointing=true
+
+            # Eval
+            CUDA_VISIBLE_DEVICES=0 python src/eval.py \
+            experiment=eval/tofu/default.yaml \
+            forget_split=${forget_split} \
+            holdout_split=${holdout_split} \
+            model=${model} \
+            task_name=${task_name} \
+            model.model_args.pretrained_model_name_or_path=saves/unlearn/${task_name} \
+            paths.output_dir=saves/unlearn/${task_name}/evals \
+            retain_logs_path=saves/eval/tofu_${model}_${retain_split}/TOFU_EVAL.json
+        done
+    done
+done
