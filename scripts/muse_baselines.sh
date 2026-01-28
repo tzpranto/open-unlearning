6fa601c2b1b621beb82b53719767216832a11da9
Md Toufikuzzaman <md.toufikzaman@gmail.com>
2026-01-28 18:45:53 +0000

    sibl main port added for muse

--- scripts/muse_baselines.sh
+++ scripts/muse_baselines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
-# echo "Master Port: $MASTER_PORT"
+export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
+echo "Master Port: $MASTER_PORT"
 
 
 per_device_train_batch_size=8
