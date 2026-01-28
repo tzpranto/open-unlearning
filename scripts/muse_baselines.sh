e87a6e9e86cae4af0ae9b109cdb7c287245483cb
Md Toufikuzzaman <md.toufikzaman@gmail.com>
2026-01-28 18:22:43 +0000

    eval test

--- scripts/muse_baselines.sh
+++ scripts/muse_baselines.sh
@@ -45,18 +45,18 @@ for data_split in "${data_splits[@]}"; do
 
         task_name=muse_${model}_${data_split}_${trainer}
 
-        CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file configs/accelerate/default_config.yaml --main_process_port $MASTER_PORT \
-        src/train.py --config-name=unlearn.yaml \
-        experiment=${experiment} \
-        model=${model} \
-        data_split=${data_split} \
-        trainer=${trainer} \
-        task_name=${task_name} \
-        retain_logs_path=saves/eval/muse_${model}_${data_split}_retrain/MUSE_EVAL.json \
-        trainer.args.per_device_train_batch_size=${per_device_train_batch_size} \
-        trainer.args.gradient_accumulation_steps=${gradient_accumulation_steps} \
-        trainer.args.ddp_find_unused_parameters=true \
-        trainer.args.gradient_checkpointing=true
+        # CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file configs/accelerate/default_config.yaml --main_process_port $MASTER_PORT \
+        # src/train.py --config-name=unlearn.yaml \
+        # experiment=${experiment} \
+        # model=${model} \
+        # data_split=${data_split} \
+        # trainer=${trainer} \
+        # task_name=${task_name} \
+        # retain_logs_path=saves/eval/muse_${model}_${data_split}_retrain/MUSE_EVAL.json \
+        # trainer.args.per_device_train_batch_size=${per_device_train_batch_size} \
+        # trainer.args.gradient_accumulation_steps=${gradient_accumulation_steps} \
+        # trainer.args.ddp_find_unused_parameters=true \
+        # trainer.args.gradient_checkpointing=true
 
         CUDA_VISIBLE_DEVICES=0 python src/eval.py \
         experiment=${experiment} \
